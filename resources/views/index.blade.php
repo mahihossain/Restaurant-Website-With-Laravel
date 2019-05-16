59a998a8639402fa4f73b02c9cc7065522903f6e
bhuban <bhuban.bogra@gmail.com>
2019-05-16 21:16:32 +0600

    Preloader Added-BBN

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Artrium</title>
 
     <!-- myCss -->
@@ -13,7 +15,12 @@
 
 </head>
 <body>
+        <!-- Preloader Start -->
+    <div class="row " id="preloader">
+      <div id="status" class="col-sm-12">&nbsp;</div>
+    </div>
 
+      <!-- Preloader End -->
 
 
 
