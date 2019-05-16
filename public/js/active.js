ece366b51f69d1965a5315b9d90cf3f972ed67fa
bhuban <bhuban.bogra@gmail.com>
2019-05-17 03:56:59 +0600

    final try-vdo-css-js updated

--- public/js/active.js
+++ public/js/active.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 /* =========================================
                 Preloader
 ============================================ */
@@ -6,10 +6,4 @@ $(window).on('load', function () { // makes sure that whole site is loaded
     $('#status').fadeOut();
     $('#preloader').delay(350).fadeOut('slow');
 });
-=======
-$(document).ready(function(){
-    $("#flip").click(function(){
-      $("#panel").slideDown("slow");
-    });
-  });
->>>>>>> frontend
+
