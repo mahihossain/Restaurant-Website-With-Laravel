1945ee088ec8be2ce63e25387407ee97fa2c2ef6
bhuban <bhuban.bogra@gmail.com>
2019-05-17 03:05:10 +0600

    1st part beta

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,12 +5,13 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Artrium</title>
 
+        <!-- font -->
+        <link href="https://fonts.googleapis.com/css?family=Yanone+Kaffeesatz" rel="stylesheet"> 
+    <!-- bootsratp -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <!-- myCss -->
         <link rel="stylesheet" href="{{ asset('css/style.css') }}">
         <link rel="stylesheet" href="{{ asset('css/responsive.css') }}">
-
-    <!-- bootsratp -->
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
 
 </head>
@@ -19,12 +20,79 @@
     <div class="row " id="preloader">
       <div id="status" class="col-sm-12">&nbsp;</div>
     </div>
-
       <!-- Preloader End -->
+         
+     
+     <header>
+         <div class="navDiv">
+            <nav class="navbar navbar-expand-lg navbar-light">   <!-- "fixed-top" class for fixed in the top -->
+              <!-- <a class="navbar-brand" href="#"> 
+                <img src="/docs/4.0/assets/brand/bootstrap-solid.svg" width="30" height="30" alt="">
+              </a> --> <!-- this for logo -->
+              <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+
+              <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                  <li class="nav-item ">
+                    <a class="nav-link test" href="#">Home <span class="sr-only">(current)</span></a>
+                  </li>
+                  <li class="nav-item ">
+                    <a class="nav-link" href="#">Menu <span class="sr-only">(current)</span></a>
+                  </li>
+                  <li class="nav-item ">
+                    <a class="nav-link" href="#">Catering <span class="sr-only">(current)</span></a>
+                  </li>
+                  <li class="nav-item ">
+                    <a class="nav-link" href="#">Events <span class="sr-only">(current)</span></a>
+                  </li>
+                  <li class="nav-item ">
+                    <a class="nav-link" href="#">Our story <span class="sr-only">(current)</span></a>
+                  </li>
+                  <li class="nav-item ">
+                    <a class="nav-link" href="#">FAQ <span class="sr-only">(current)</span></a>
+                  </li>
+                </ul>
+                
+              </div>
+            </nav>
+         </div>
+        <!--  <div id="">   try to put image on top of video
+           <img src="{{ asset('image/logo/logo.jpg') }}">
+         </div> -->
+      <div class="overlay"></div>
+      <video poster="{{ asset('video/bg.jpg') }}" playsinline="playsinline" autoplay="autoplay" muted="muted" loop="loop">
+        <source src="{{ asset('video/food.mp4') }}" type="video/mp4">
+      </video>
+
+    </header>
 
 
 
+      <section >
+        <div class="container">
+            <div class="row">
+                 <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+          cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+          proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h1>
+            </div>
+            
+        </div>
+         
+      </section>
 
+      <section >
+          <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+          tempor incididunt ut lmarabore et dolore magna aliqua. Ut enim ad minim veniam,
+          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+          cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+          proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h1>
+      </section>
 
 
 
