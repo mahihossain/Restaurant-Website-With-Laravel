8452d2647b7cf3f45746a52f98698c3a31d14d42
HellBlazer616 <rifat109@gmail.com>
2019-05-17 04:42:41 +0600

    primitive design of the middle section

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -68,9 +68,63 @@
 
     </header>
 
+    <div class="container-fluid">
+      <div id="reverse" class="row">
+          <div class="col col align-self-center">   
+              <p class=" text-justify text-center">
+                  Croissant donut cake lemon drops cake candy pudding. Lollipop I love topping jelly beans bonbon. Tart dessert croissant lemon drops biscuit bear claw sugar plum donut I love. Cotton candy pie cake liquorice wafer powder jujubes bonbon.
+              </p>
+          </div>
+          <div id="squeeze" class="col-sm-6">
+              <img class="img-fluid rounded float-right myImg left-image" alt="Responsive image" src="{{asset('image/appetizer-bowl-delicious.jpg')}}">
+          </div>
+      </div>
+  </div>
 
+  <div class="container-fluid second">
+      <div class="row">
+          <div class="col-sm-7">
+              <img class="img-fluid rounded float-right myImg right-image" alt="Responsive image" src="{{asset('image/appetizer-bowl-delicious.jpg')}}">
+          </div>
+          <div class="col col align-self-center">
+              <p class=" text-justify text-center">
+                  Croissant donut cake lemon drops cake candy pudding. Lollipop I love topping jelly beans bonbon. Tart dessert croissant lemon drops biscuit bear claw sugar plum donut I love. Cotton candy pie cake liquorice wafer powder jujubes bonbon.
+              </p>
+          </div>
+      </div>
+   </div>
 
-      <section >
+   <div class="container-fluid third">
+          <div id="reverse" class="row">
+              <div class="col col align-self-center">
+                  <p class=" text-justify text-center">
+                      Croissant donut cake lemon drops cake candy pudding. Lollipop I love topping jelly beans bonbon. Tart dessert croissant lemon drops biscuit bear claw sugar plum donut I love. Cotton candy pie cake liquorice wafer powder jujubes bonbon.
+                  </p>
+              </div>
+              <div class="col-sm-8">
+                  <img class="img-fluid rounded float-right myImg right-image" alt="Responsive image" src="{{asset('image/appetizer-bowl-delicious.jpg')}}">
+              </div>
+          </div>
+       </div>
+
+       
+      <div class="row" id="footer">
+          <div class="col col align-self-center">
+              <p class=" text-justify text-center">
+                  Muffin I love jelly beans pudding. I love donut chocolate bar topping. Apple pie croissant toffee lollipop lollipop fruitcake tootsie roll biscuit. Oat cake cookie I love bonbon apple pie pudding oat cake carrot cake marzipan.
+              </p>
+          </div>
+          <div class="col">
+              <a href="https://www.google.com/maps/place/The+Atrium+Restaurant/@23.8033073,90.4192192,15z/data=!4m5!3m4!1s0x3755c7b1c6ccc04b:0xa6f2a07bd56571af!8m2!3d23.802477!4d90.4228988"  target="_blank"><img class="img-fluid "id="location" src="{{asset('image/atruim-location.png')}}" alt=""></a>
+          </div>
+          <div class="col col align-self-center">
+              <p class=" text-justify text-center">
+                  Sugar plum macaroon I love sweet caramels. Muffin dragée halvah pastry gummies biscuit candy canes jelly-o donut. Carrot cake jelly-o croissant I love I love donut cake cheesecake wafer.
+              </p>
+          </div>
+      </div>
+
+      {{-- <section > // should we keep these --DHRUBO
         <div class="container">
             <div class="row">
                  <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
@@ -92,7 +146,7 @@
           consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
           cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
           proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h1>
-      </section>
+      </section> --}}
 
 
 
@@ -108,5 +162,6 @@
 
     <!-- myJS -->
         <script src="{{ asset('js/active.js') }}"></script>
-
+  
+</body>
 </html>
