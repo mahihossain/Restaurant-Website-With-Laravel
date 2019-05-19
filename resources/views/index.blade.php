9bf9fa57cd0292be38bfffd9928b29571481d813
bhuban <bhuban.bogra@gmail.com>
2019-05-19 19:21:26 +0600

    VDO change..

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -25,7 +25,7 @@
      
      <header>
          <div class="navDiv">
-            <nav class="navbar navbar-expand-lg navbar-light">   <!-- "fixed-top" class for fixed in the top -->
+            <nav class="navbar navbar-expand-lg navbar-light fixed-top">   <!-- "fixed-top" class for fixed in the top -->
               <!-- <a class="navbar-brand" href="#"> 
                 <img src="/docs/4.0/assets/brand/bootstrap-solid.svg" width="30" height="30" alt="">
               </a> --> <!-- this for logo -->
@@ -62,8 +62,8 @@
            <img src="{{ asset('image/logo/logo.jpg') }}">
          </div> -->
       <div class="overlay"></div>
-      <video poster="{{ asset('video/bg.jpg') }}" playsinline="playsinline" autoplay="autoplay" muted="muted" loop="loop">
-        <source src="{{ asset('video/Mongolian.mp4') }}" type="video/mp4">
+      <video poster="{{ asset('video/bg.jpg') }}" playsinline="playsinline" autoplay="autoplay" muted="muted" loop="loop" >
+        <source src="{{ asset('video/mergeFood720p.mp4') }}" type="video/mp4">
       </video>
     </header>
 
