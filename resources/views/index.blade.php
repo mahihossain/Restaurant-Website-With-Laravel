676482d59092b84afd4031e6ce6591b5e67cdb00
bhuban <bhuban.bogra@gmail.com>
2019-05-23 17:20:21 +0600

    review on progress

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -350,11 +350,20 @@
                   <div class="container-fluid">
                     <div class="row">
                       <div class="col-sm-6">
-                        
+                        <img class="img-fluid image" src="{{asset('image/review/r.png')}}">
                       </div>
                       <div class="col-sm-6">
-                        
+                        <h3>Bhuban</h3>
+                        <p>Bhubanbbnn</p>
                       </div>
+                    </div>
+                    <div class="row">
+                      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                      consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                      cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                      proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                     </div>
                   </div>
 
