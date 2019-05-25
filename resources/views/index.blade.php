552f59a816c568937420c6f4fd049d0c4597e22b
HellBlazer616 <rifat109@gmail.com>
2019-05-25 16:08:06 +0600

    Smooth Scrolling and Positioned Svgs

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -564,7 +564,7 @@
         <script src="{{ asset('js/owl-carousel/owl.carousel.min.js') }}"></script>
 
         {{------------------- Scroll Magic------------> --}}
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/2.1.3/TweenLite.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/2.1.3/TweenMax.min.js"></script>
 
 
     <!-- myJS -->
