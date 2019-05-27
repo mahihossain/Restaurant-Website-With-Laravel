40499c11ebf2203af48c7940e2fd014bdda8a99a
HellBlazer616 <rifat109@gmail.com>
2019-05-28 02:35:51 +0600

    Changed Review Header

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -339,7 +339,7 @@
           <div class="container seventh">
             <div class="row align-self-center">
               <div class="col-sm-12">
-                <h2 class="review-header wow zoomIn " data-wow-offset="12" data-wow-duration="1.5s"  data-wow-delay=".1s">Our Customers Say About Us</h2>
+                <h2 class="review-header wow zoomIn " data-wow-offset="12" data-wow-duration="1.5s"  data-wow-delay=".1s">What Our Customers Say About Us</h2>
                 <div id="myCarousel" class="carousel slide" data-ride="carousel">
                   <!-- Carousel indicators -->
                   <ol class="carousel-indicators">
