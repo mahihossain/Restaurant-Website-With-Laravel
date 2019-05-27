2642e3385a6772860c25710ecd4fc38858c8b4df
HellBlazer616 <rifat109@gmail.com>
2019-05-27 21:00:52 +0600

    Animation on repeat on page scroll up and then down

--- public/js/active.js
+++ public/js/active.js
@@ -147,3 +147,12 @@ window.addEventListener("scroll",parallaxtwo,false)
 
 
 //////////////////////////////////
+
+window.addEventListener('scroll', function(e) {
+
+  if( $(window).scrollTop() <= 50) {
+      $('.wow').removeClass('animated');
+      $('.wow').removeAttr('style');
+      new WOW().init();
+  }
+});
