00bafd8c9b8a258ea470cefda259da2a15c926e3
bhuban <bhuban.bogra@gmail.com>
2019-05-30 00:53:11 +0600

    slider added test-bbn

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -1,123 +1,62 @@
-<link href="//netdna.bootstrapcdn.com/bootstrap/3.0.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-<script src="//netdna.bootstrapcdn.com/bootstrap/3.0.1/js/bootstrap.min.js"></script>
-<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
-<!------ Include the above in your HEAD tag ---------->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+  <style type="text/css">
+      .t {
+  height: 65vh;
+  min-height: 350px;
+  background: no-repeat center center scroll;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+}
+  </style>
+</head>
+<body>
 
-<link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.0.2/css/bootstrap.min.css">
-<link rel="stylesheet" href="http://blueimp.github.io/Gallery/css/blueimp-gallery.min.css">
-<link rel="stylesheet" href="https://raw.github.com/blueimp/Bootstrap-Image-Gallery/master/css/bootstrap-image-gallery.css">
-<link rel="stylesheet" href="https://raw.github.com/blueimp/Bootstrap-Image-Gallery/master/css/demo.css">
+<div class="container-fluid">
+   
+  <div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+      <li data-target="#myCarousel" data-slide-to="2"></li>
+    </ol>
 
-<style type="text/css">
-</style>
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner">
+      <div class="item active t">
+        <img src="{{asset('image/galleryimg/F9.jpg')}}" alt="Los Angeles" style="width:100%;">
+      </div>
 
-<div class="gallery">
-    <div class="container">
-    <!-- The container for the list of example images -->
-    <div class="row">
-        <div class="">
-            <div id="links">
-        <a data-gallery="" title="Natalia" href="http://farm4.static.flickr.com/3812/11188050815_5e8cccf0f6_b.jpg">
-            <img src="http://farm4.static.flickr.com/3812/11188050815_5e8cccf0f6_s.jpg">
-        </a>
-        <a data-gallery="" title="Delft Touch in the kitchen" href="http://farm4.static.flickr.com/3812/11188050815_5e8cccf0f6_b.jpg">
-            <img src="http://farm4.static.flickr.com/3812/11188050815_5e8cccf0f6_s.jpg">
-        </a>
-        <a data-gallery="" title="Father Figure Project: The Social Construct" href="http://farm4.static.flickr.com/3703/11191153143_04bbbc8301_b.jpg">
-            <img src="http://farm4.static.flickr.com/3703/11191153143_04bbbc8301_s.jpg">
-        </a>
-        <a data-gallery="" title="Higherbeams." href="http://farm4.static.flickr.com/3821/11190956343_09b8022ea2_b.jpg">
-            <img src="http://farm4.static.flickr.com/3821/11190956343_09b8022ea2_s.jpg">
-        </a>
-        <a data-gallery="" title="Contact" href="http://farm8.static.flickr.com/7325/11186917235_003d9df5d6_b.jpg">
-            <img src="http://farm8.static.flickr.com/7325/11186917235_003d9df5d6_s.jpg">
-        </a>
-        <a data-gallery="" title="The Track" href="http://farm6.static.flickr.com/5501/11186913445_c88dd25c59_b.jpg">
-            <img src="http://farm6.static.flickr.com/5501/11186913445_c88dd25c59_s.jpg">
-        </a>
-        <a data-gallery="" title="TheHallsOfAdoration" href="http://farm4.static.flickr.com/3715/11186895035_8596236621_b.jpg">
-            <img src="http://farm4.static.flickr.com/3715/11186895035_8596236621_s.jpg">
-        </a>
-        <a data-gallery="" title="Si me dijeran pide un deseo" href="http://farm8.static.flickr.com/7414/11190086653_e330723a8c_b.jpg">
-            <img src="http://farm8.static.flickr.com/7414/11190086653_e330723a8c_s.jpg">
-        </a>
-        <a data-gallery="" title="untitled" href="http://farm3.static.flickr.com/2891/11188743634_f339e72b79_b.jpg">
-            <img src="http://farm3.static.flickr.com/2891/11188743634_f339e72b79_s.jpg">
-        </a>
-        <a data-gallery="" title="Loch Ba" href="http://farm8.static.flickr.com/7390/11187408694_9dd7188ca8_b.jpg">
-            <img src="http://farm8.static.flickr.com/7390/11187408694_9dd7188ca8_s.jpg">
-        </a>
-        <a data-gallery="" title="Remarkable Rocks 2" href="http://farm3.static.flickr.com/2831/11187401055_1e498bde13_b.jpg">
-            <img src="http://farm3.static.flickr.com/2831/11187401055_1e498bde13_s.jpg">
-        </a>
-        <a data-gallery="" title="The imagination tree" href="http://farm3.static.flickr.com/2831/11187401055_1e498bde13_b.jpg">
-            <img src="http://farm3.static.flickr.com/2831/11187401055_1e498bde13_s.jpg">
-        </a>
-        <a data-gallery="" title="Blue Patricia Lake" href="http://farm4.static.flickr.com/3720/11190734053_7cf9749bdb_b.jpg">
-            <img src="http://farm4.static.flickr.com/3720/11190734053_7cf9749bdb_s.jpg">
-        </a>
-        <a data-gallery="" title="Goblin Brute" href="http://farm4.static.flickr.com/3740/11186937956_d8d8422169_b.jpg">
-            <img src="http://farm4.static.flickr.com/3740/11186937956_d8d8422169_s.jpg">
-        </a>
-        <a data-gallery="" title="." href="http://farm8.static.flickr.com/7421/11191341825_b2bf635f25_b.jpg">
-            <img src="http://farm8.static.flickr.com/7421/11191341825_b2bf635f25_s.jpg">
-        </a>
-        <a data-gallery="" title="be the light 336.365" href="http://farm4.static.flickr.com/3788/11193226453_fd9b06008c_b.jpg">
-            <img src="http://farm4.static.flickr.com/3788/11193226453_fd9b06008c_s.jpg">
-        </a>
-        <a data-gallery="" title="Lofoten Islands" href="http://farm4.static.flickr.com/3788/11193226453_fd9b06008c_b.jpg">
-            <img src="http://farm4.static.flickr.com/3788/11193226453_fd9b06008c_s.jpg">
-        </a>
-        <a data-gallery="" title="もみじ" href="http://farm4.static.flickr.com/3737/11189043544_e992a227d3_b.jpg">
-            <img src="http://farm4.static.flickr.com/3737/11189043544_e992a227d3_s.jpg">
-        </a>
-        <a data-gallery="" title="Nocturna en Albarracín./ Nocturne near Albarracín (Teruel, Spain)." href="http://farm8.static.flickr.com/7455/11187819606_c008b36106_b.jpg">
-            <img src="http://farm8.static.flickr.com/7455/11187819606_c008b36106_s.jpg">
-        </a>
+      <div class="item t">
+        <img src="{{asset('image/galleryimg/F9.jpg')}}" alt="Chicago" style="width:100%;">
+      </div>
+    
+      <div class="item t">
+        <img src="{{asset('image/galleryimg/F9.jpg')}}" alt="New york" style="width:100%;">
+      </div>
     </div>
-        </div>
-    </div>
-    <br>
-</div>
+
+    <!-- Left and right controls -->
+    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
 </div>
 
-<!-- The Bootstrap Image Gallery lightbox, should be a child element of the document body -->
-<div id="blueimp-gallery" class="blueimp-gallery">
-    <!-- The container for the modal slides -->
-    <div class="slides"></div>
-    <!-- Controls for the borderless lightbox -->
-    <h3 class="title"></h3>
-    <a class="prev">‹</a>
-    <a class="next">›</a>
-    <a class="close">×</a>
-    <a class="play-pause"></a>
-    <ol class="indicator"></ol>
-    <!-- The modal dialog, which will be used to wrap the lightbox content -->
-    <div class="modal fade">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <button type="button" class="close" aria-hidden="true">×</button>
-                    <h4 class="modal-title"></h4>
-                </div>
-                <div class="modal-body next"></div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-default pull-left prev">
-                        <i class="glyphicon glyphicon-chevron-left"></i>
-                        Previous
-                    </button>
-                    <button type="button" class="btn btn-primary next">
-                        Next
-                        <i class="glyphicon glyphicon-chevron-right"></i>
-                    </button>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-<!-- Bootstrap JS is not required, but included for the responsive demo navigation and button states -->
-<script src="http://netdna.bootstrapcdn.com/bootstrap/3.0.2/js/bootstrap.min.js"></script>
-<script src="http://blueimp.github.io/Gallery/js/jquery.blueimp-gallery.min.js"></script>
-<script src="https://raw.github.com/blueimp/Bootstrap-Image-Gallery/master/js/bootstrap-image-gallery.js"></script>
-<script src="https://raw.github.com/blueimp/Bootstrap-Image-Gallery/master/js/demo.js"></script>
+</body>
+</html>
