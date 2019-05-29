098c99930cc76dae65a6cdcffab00c3862e44a9d
bhuban <bhuban.bogra@gmail.com>
2019-05-30 00:04:36 +0600

    work on animation

--- public/js/active.js
+++ public/js/active.js
@@ -33,14 +33,14 @@ $(function () {
     new WOW().init();
 });
 
-window.addEventListener('scroll', function(e) {
+// window.addEventListener('scroll', function(e) {
 
-  if( $(window).scrollTop() <= 50) {
-      $('.wow').removeClass('animated');
-      $('.wow').removeAttr('style');
-      new WOW().init();
-  }
-});
+//   if( $(window).scrollTop() <= 50) {
+//       $('.wow').removeClass('animated');
+//       $('.wow').removeAttr('style');
+//       new WOW().init();
+//   }
+// });
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
