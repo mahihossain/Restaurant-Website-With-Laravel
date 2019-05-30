b51e08b7f095bd257627c5e451af98f5e24243de
mahihossain <sayed.muddashir@gmail.com>
2019-05-31 02:14:02 +0600

    Added logo in nav bar

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -38,6 +38,7 @@
               <!-- <a class="navbar-brand" href="#"> 
                 <img src="/docs/4.0/assets/brand/bootstrap-solid.svg" width="30" height="30" alt="">
               </a> --> <!-- this for logo -->
+              <img src="{{ asset('image/logo/logo.png') }}" class="logo_size">
               <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
               </button>
