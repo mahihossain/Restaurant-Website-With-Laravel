0413d3bdf83a5d3bc6c792719779e56971aa9e3e
HellBlazer616 <rifat109@gmail.com>
2019-05-31 01:03:34 +0600

    center

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -43,7 +43,7 @@
               </button>
 
               <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav mr-auto">
+                <ul class="navbar-nav.justify-content-center">
                   <li class="nav-item ">
                     <a class="nav-link test" href="#">Home <span class="sr-only">(current)</span></a>
                   </li>
